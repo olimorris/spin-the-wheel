5f6ca2ddb93c45d2b1be24e56344c6d9623d382f
Oli Morris <olimorris@users.noreply.github.com>
2023-11-01 18:09:04 +0000

    chore: undo seperation of css and js

---
+++ src/index.js
@@ -0,0 +1,111 @@
+const sectors = [
+  { color: "#0B1413", text: "#1FDE5C", label: "Sweets" },
+  { color: "#B9C6BF", text: "#0B1413", label: "Prize draw" },
+  { color: "#0B1413", text: "#1FDE5C", label: "Sweets" },
+  { color: "#B9C6BF", text: "#0B1413", label: "Prize draw!" },
+  { color: "#0B1413", text: "#1FDE5C", label: "Sweets + Prize draw!" },
+  { color: "#B9C6BF", text: "#0B1413", label: "You lose" }
+];
+
+const events = {
+  listeners: {},
+  addListener: function (eventName, fn) {
+    this.listeners[eventName] = this.listeners[eventName] || [];
+    this.listeners[eventName].push(fn);
+  },
+  fire: function (eventName, ...args) {
+    if (this.listeners[eventName]) {
+      for (let fn of this.listeners[eventName]) {
+        fn(...args);
+      }
+    }
+  }
+};
+
+const rand = (m, M) => Math.random() * (M - m) + m;
+const tot = sectors.length;
+const spinEl = document.querySelector("#spin");
+const ctx = document.querySelector("#wheel").getContext("2d");
+const dia = ctx.canvas.width;
+const rad = dia / 2;
+const PI = Math.PI;
+const TAU = 2 * PI;
+const arc = TAU / sectors.length;
+
+const friction = 0.991; // 0.995=soft, 0.99=mid, 0.98=hard
+let angVel = 0; // Angular velocity
+let ang = 0; // Angle in radians
+
+let spinButtonClicked = false;
+
+const getIndex = () => Math.floor(tot - (ang / TAU) * tot) % tot;
+
+function drawSector(sector, i) {
+  const ang = arc * i;
+  ctx.save();
+
+  // COLOR
+  ctx.beginPath();
+  ctx.fillStyle = sector.color;
+  ctx.moveTo(rad, rad);
+  ctx.arc(rad, rad, rad, ang, ang + arc);
+  ctx.lineTo(rad, rad);
+  ctx.fill();
+
+  // TEXT
+  ctx.translate(rad, rad);
+  ctx.rotate(ang + arc / 2);
+  ctx.textAlign = "right";
+  ctx.fillStyle = sector.text;
+  ctx.font = "bold 30px 'Lato', sans-serif";
+  ctx.fillText(sector.label, rad - 10, 10);
+  //
+
+  ctx.restore();
+}
+
+function rotate() {
+  const sector = sectors[getIndex()];
+  ctx.canvas.style.transform = `rotate(${ang - PI / 2}rad)`;
+
+  spinEl.textContent = !angVel ? "SPIN" : sector.label;
+  spinEl.style.background = sector.color;
+  spinEl.style.color = sector.text;
+}
+
+function frame() {
+  // Fire an event after the wheel has stopped spinning
+  if (!angVel && spinButtonClicked) {
+    const finalSector = sectors[getIndex()];
+    events.fire("spinEnd", finalSector);
+    spinButtonClicked = false; // reset the flag
+    return;
+  }
+
+  angVel *= friction; // Decrement velocity by friction
+  if (angVel < 0.002) angVel = 0; // Bring to stop
+  ang += angVel; // Update angle
+  ang %= TAU; // Normalize angle
+  rotate();
+}
+
+function engine() {
+  frame();
+  requestAnimationFrame(engine);
+}
+
+function init() {
+  sectors.forEach(drawSector);
+  rotate(); // Initial rotation
+  engine(); // Start engine
+  spinEl.addEventListener("click", () => {
+    if (!angVel) angVel = rand(0.25, 0.45);
+    spinButtonClicked = true;
+  });
+}
+
+init();
+
+events.addListener("spinEnd", (sector) => {
+  console.log(`Woop! You won ${sector.label}`);
+});
